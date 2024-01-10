d0edb77e910a8c6d35e3dc67988ec850a1502955
mmvergara <mark.jesusmanabat@gmail.com>
2024-01-10 20:15:45 +0800

    outline

--- cogs/text_commands.py
+++ cogs/text_commands.py
@@ -1,16 +1,14 @@
 from discord.ext import commands
 
+
 class MyCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def ping(self, ctx):
-        await ctx.send('Pong!')
+    async def show(self, ctx: commands.Context):
+        await ctx.send("Showing all projects")
 
-    @commands.command()
-    async def show(self, ctx):
-        await ctx.send('Showing all projects')
 
 async def setup(bot):
     await bot.add_cog(MyCommands(bot))
--- main.py
+++ main.py
@@ -1,13 +1,48 @@
 import os
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv,find_dotenv
-bot = commands.Bot(command_prefix='td ',intents=discord.Intents.all())
+from discord import app_commands
+from dotenv import load_dotenv, find_dotenv
+from firebase import connect_user
+
+test_guild = 910166218181345320
+bot = commands.Bot(command_prefix="td ", intents=discord.Intents.all())
+
 
 @bot.event
 async def on_ready():
-  await bot.load_extension("commands.text-commands")
-  print('Bot is ready!')
+    await bot.load_extension("cogs.text_commands")
+    try:
+        # await bot.tree.sync(guild=discord.Object(id=test_guild))
+        print("Synced")
+    except Exception as e:
+        print(e)
+    print("Bot is ready!")
+
+
+@bot.tree.command(
+    name="ping",
+    description="Shows the bot latency",
+    guild=discord.Object(id=test_guild),
+)
+async def ping(interaction):
+    await interaction.response.send_message(f"Pong! {round(bot.latency * 1000)}ms")
+
+
+# take 1 argument, a project name
+@bot.tree.command(
+    name="connect",
+    description="Connect to a project",
+    guild=discord.Object(id=test_guild),
+)
+@app_commands.describe(project_key="Project Key")
+async def connect(interaction, project_key: str):
+    try:
+        await connect_user(interaction.user.id, project_key)
+        await interaction.response.send_message(f"Connected to {project_key}")
+    except Exception as e:
+        print(e)
+        await interaction.response.send_message(f"Error Connecting to {project_key}")
 
 
 # Load the .env file
@@ -16,5 +51,3 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Run the bot
 bot.run(token=BOT_TOKEN)
-
-
