7553867b88845e17f0c12fca381162be6421188a
mmvergara <mark.jesusmanabat@gmail.com>
2024-01-13 12:32:48 +0800

    update deshow embed

--- helpers/embed_templates.py
+++ helpers/embed_templates.py
@@ -8,10 +8,11 @@ def create_project_embed(project: Project) -> Embed:
         description=f"----------------------------",
         color=0xDD7C36,
     )
-
+    empty_sections = []
     for section_id, section in project.sections.items():
         # Skip empty sections
         if len(section["tasks"]) == 0:
+            empty_sections.append([section["sectionName"], section_id])
             continue
 
         section_str = ""
@@ -23,6 +24,16 @@ def create_project_embed(project: Project) -> Embed:
             value=section_str,
             inline=False,
         )
+
+    # Add empty sections
+    empty_sections_str = ""
+    for section in empty_sections:
+        empty_sections_str += f"{section[0]} - #{section[1]}\n"
+    embed.add_field(
+        name=f"Empty Sections",
+        value=empty_sections_str,
+        inline=False,
+    )
 
     return embed
 
